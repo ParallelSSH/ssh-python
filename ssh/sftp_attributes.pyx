b3c60fc7d8cd437519bf8a96f0510c2e7588638e
Pan <zuboci@yandex.com>
2018-06-25 20:32:34 +0100

    Added attribute properties.

--- ssh/sftp_attributes.pyx
+++ ssh/sftp_attributes.pyx
@@ -47,3 +47,105 @@ cdef class SFTPAttributes:
         if _attrs is NULL:
             raise MemoryError
         return SFTPAttributes.from_ptr(_attrs, sftp)
+
+    @property
+    def name(self):
+        if self._attrs is NULL:
+            return
+        cdef bytes b_name = self._attrs.name
+        return b_name
+
+    @property
+    def longname(self):
+        if self._attrs is NULL:
+            return
+        cdef bytes b_longname = self._attrs.longname
+        return b_longname
+        
+    @property
+    def flags(self):
+        return self._attrs.flags if self._attrs is not NULL else None
+
+    @property
+    def type(self):
+        return self._attrs.type if self._attrs is not NULL else None
+
+    @property
+    def size(self):
+        return self._attrs.size if self._attrs is not NULL else None
+
+    @property
+    def uid(self):
+        return self._attrs.uid if self._attrs is not NULL else None
+
+    @property
+    def gid(self):
+        return self._attrs.gid if self._attrs is not NULL else None
+
+    @property
+    def owner(self):
+        if self._attrs is NULL:
+            return
+        cdef bytes b_owner = self._attrs.owner \
+            if self._attrs.owner is not NULL else None
+        return b_owner
+
+    @property
+    def group(self):
+        if self._attrs is NULL:
+            return
+        cdef bytes b_group = self._attrs.group \
+            if self._attrs.group is not NULL else None
+        return b_group
+
+    @property
+    def permissions(self):
+        return self._attrs.permissions if self._attrs is not NULL else None
+
+    @property
+    def atime64(self):
+        return self._attrs.atime64 if self._attrs is not NULL else None
+
+    @property
+    def atime(self):
+        return self._attrs.atime if self._attrs is not NULL else None
+
+    @property
+    def atime_nseconds(self):
+        return self._attrs.atime_nseconds if self._attrs is not NULL else None
+
+    @property
+    def createtime(self):
+        return self._attrs.createtime if self._attrs is not NULL else None
+
+    @property
+    def createtime_nseconds(self):
+        return self._attrs.createtime_nseconds if self._attrs is not NULL else None
+
+    @property
+    def mtime64(self):
+        return self._attrs.mtime64 if self._attrs is not NULL else None
+
+    @property
+    def mtime(self):
+        return self._attrs.mtime if self._attrs is not NULL else None
+
+    @property
+    def mtime_nseconds(self):
+        return self._attrs.mtime_nseconds if self._attrs is not NULL else None
+
+    # @property
+    # def acl(self):
+    #     return self._attrs.acl if self._attrs is not NULL else None
+
+    @property
+    def extended_count(self):
+        return self._attrs.extended_count if self._attrs is not NULL else None
+
+    # @property
+    # def extended_type(self):
+    #     return self._attrs.extended_type if self._attrs is not NULL else None
+
+    # @property
+    # def extended_data(self):
+    #     return self._attrs.extended_data if self._attrs is not NULL else None
