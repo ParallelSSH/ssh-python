95dfc9f8a30ae9274293f64d310c3ae7f2dfd4bb
Pan <zuboci@yandex.com>
2019-08-19 13:10:27 +0100

    Move platform check to setup.py.

--- ssh/__init__.pyx
+++
@@ -1,5 +0,0 @@
-from platform import python_version_tuple, system
-if system() == 'Windows' and python_version_tuple()[0] == "2":
-    raise ImportError("ssh-python for Python 2 on Windows is unsupported - "
-                      "Python 3 or above is required.")
-del python_version_tuple, system
