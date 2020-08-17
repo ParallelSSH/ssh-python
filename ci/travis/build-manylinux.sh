2ea7b4afcad6d891c3bf94c22dc359bd9db7b045
Panos <zuboci@yandex.com>
2020-08-18 00:33:17 +0100

    Updated cfg

--- ci/travis/build-manylinux.sh
+++ ci/travis/build-manylinux.sh
@@ -2,10 +2,10 @@
 
 docker_tag="parallelssh/ssh-manylinux"
 
-rm -rf build dist
+rm -rf build ssh/libssh.*
 
 docker pull $docker_tag || echo
 docker build --pull --cache-from $docker_tag ci/docker/manylinux -t $docker_tag
-# if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then docker push $docker_tag; fi
+if [[ -z "$CIRCLE_PULL_REQUEST" ]]; then docker push $docker_tag; fi
 docker run --rm -v `pwd`:/io $docker_tag /io/ci/travis/build-wheels.sh
 ls wheelhouse/
