9545ae02c058e046e07bb13d8658507af727d56b
Panos <zuboci@yandex.com>
2020-08-18 22:58:46 +0100

    Updated cfg

--- ci/osx-wheel.sh
+++ ci/osx-wheel.sh
@@ -12,7 +12,7 @@ pip3 install -U setuptools pip
 pip3 install -U delocate wheel
 pip3 wheel .
 ls -lhtr /usr/local/lib/
-# cp /usr/local/lib/libssh* .
+cp /usr/local/lib/libssh* .
 delocate-listdeps --all *.whl
 delocate-wheel -v *.whl
 delocate-listdeps --all *.whl
@@ -21,10 +21,12 @@ ls -l *.whl
 rm -f *.dylib
 pip3 install -v *.whl
 pwd; mkdir -p temp; cd temp; pwd
-python -c "from ssh.session import Session; Session()" && echo "Import successfull"
+python3 -c "from ssh.session import Session; Session()" && echo "Import successfull"
 cd ..; pwd
 set +x
 deactivate
 set -x
 
-ls -lh *.whl
+mkdir -p wheels
+mv *.whl wheels/
+ls -lh wheels
