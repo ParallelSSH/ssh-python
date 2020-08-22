cd9402be06a1aaf30d59d49a2288f826bce1e5c8
Panos <zuboci@yandex.com>
2020-08-22 12:06:01 +0100

    Updated tests

--- tests/test_sftp.py
+++ tests/test_sftp.py
@@ -31,7 +31,7 @@ from ssh.sftp_handles import SFTPDir, SFTPFile
 from ssh.sftp_attributes import SFTPAttributes
 from ssh.exceptions import InvalidAPIUse, SFTPHandleError, SFTPError
 from ssh.error_codes import SSH_AGAIN
-from ssh.helper import wait_socket
+from ssh.utils import wait_socket
 
 
 from .base_case import SSHTestCase
