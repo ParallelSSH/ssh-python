e9e43122de058f7fe97c088c6304291e3438a9f8
Panos <zuboci@yandex.com>
2020-08-26 18:41:54 +0100

    Fix pypi uploads from appveyor

--- ci/appveyor/pypi_upload.py
+++ ci/appveyor/pypi_upload.py
@@ -4,16 +4,19 @@ import os
 
 
 def upload_pypi(files):
-    repo_tag = os.environ['APPVEYOR_REPO_TAG']
+    repo_tag = os.environ.get('APPVEYOR_REPO_TAG', 'false')
     if repo_tag == 'false':
         sys.stderr.write("Not a tagged release, skipping upload" + os.linesep)
         return
-    _user, _pass = os.environ['PYPI_USER'], os.environ['PYPI_PASS']
-    try:
-        subprocess.check_call(['twine', 'upload', '-u', _user,
-                               '-p', _pass, files])
-    except Exception:
+    _user, _pass = os.environ.get('PYPI_USER'), os.environ.get('PYPI_PASS')
+    if not _user or not _pass:
+        sys.stderr.write("No PyPi credentials set" + os.linesep)
+        sys.exit(1)
+    proc = subprocess.run(['twine', 'upload', '-u', _user,
+                           '-p', _pass, files])
+    if proc.returncode:
         sys.stderr.write("Error uploading to PyPi" + os.linesep)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
