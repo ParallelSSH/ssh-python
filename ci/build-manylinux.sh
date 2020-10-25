05fac6d9da1ea1e27a26a4cef5b4d7e3ebe1d094
Panos <pkittenis@users.noreply.github.com>
2020-10-25 11:38:27 +0000

    updated circleci cfg (#33)
    * updated circleci cfg

--- ci/build-manylinux.sh
+++ ci/build-manylinux.sh
@@ -7,6 +7,6 @@ python ci/appveyor/fix_version.py .
 
 docker pull $docker_tag || echo
 docker build --pull --cache-from $docker_tag ci/docker/manylinux -t $docker_tag
-if [[ -z "$CIRCLE_PULL_REQUEST" ]]; then docker push $docker_tag; fi
+if [[ -z "$CIRCLE_PR_NUMBER" ]]; then docker push $docker_tag; fi
 docker run --rm -v `pwd`:/io $docker_tag /io/ci/build-wheels.sh
 ls wheelhouse/
