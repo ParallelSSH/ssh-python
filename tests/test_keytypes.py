a67e1de33a58302a9b26aaa924ca32c4490d10eb
Panos <zuboci@yandex.com>
2020-12-04 15:10:49 +0000

    Added keygen test

--- tests/test_keytypes.py
+++ tests/test_keytypes.py
@@ -1,9 +1,16 @@
 import unittest
 
+from ssh.key import generate
 from ssh.keytypes import key_type_from_name, RSAKey, ED25519Key, UnknownKey
 
 
 class KeyTypesTest(unittest.TestCase):
+
+    def test_keygen(self):
+        key_type = RSAKey()
+        key = generate(key_type, 1024)
+        self.assertIsInstance(key.key_type(), RSAKey)
+    
     def test_key_type_from_name(self):
         keytype = key_type_from_name('ssh-rsa')
         self.assertIsInstance(keytype, RSAKey)
