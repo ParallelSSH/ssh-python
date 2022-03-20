29f36f7bf1a95455cfc64753287c611c50c27ce4
Panos <zuboci@yandex.com>
2022-03-20 15:08:11 +0000

    Updated rtd configuration

--- setup.py
+++ setup.py
@@ -20,9 +20,7 @@ else:
 
 _PYTHON_MAJOR_VERSION = int(platform.python_version_tuple()[0])
 ON_WINDOWS = platform.system() == 'Windows'
-ON_RTD = os.environ.get('READTHEDOCS') == 'True'
-SYSTEM_LIBSSH = bool(os.environ.get('SYSTEM_LIBSSH', 0)) or \
-                ON_RTD or ON_WINDOWS
+SYSTEM_LIBSSH = bool(os.environ.get('SYSTEM_LIBSSH', 0)) or ON_WINDOWS
 
 if ON_WINDOWS and _PYTHON_MAJOR_VERSION < 3:
     raise ImportError(
@@ -65,8 +63,8 @@ cython_args = {
 
 runtime_library_dirs = ["$ORIGIN/."] if not SYSTEM_LIBSSH else None
 _lib_dir = os.path.abspath("./local/lib") if not SYSTEM_LIBSSH else "/usr/local/lib"
-include_dirs = ["./local/include", "./libssh/include"] if (ON_WINDOWS or ON_RTD) or \
-               not SYSTEM_LIBSSH else ["/usr/local/include"]
+include_dirs = ["./local/include", "./libssh/include"] \
+    if ON_WINDOWS or not SYSTEM_LIBSSH else ["/usr/local/include"]
 
 extensions = [
     Extension(
