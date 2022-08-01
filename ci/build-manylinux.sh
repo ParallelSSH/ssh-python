3f838cb5ba8a40b5316d7791b59afc922f934914
Panos <pkittenis@users.noreply.github.com>
2022-08-01 21:02:54 +0100

    Manylinux 2_24 and 2_28 wheels for aarch64 (ARM64) and x86_64 (#56)
    * Updated CI cfg, setup.py
    * Re-generated C files with latest cython
    * Added manylinux 2_24 and 2_28 docker builds aarch64 (ARM64) and x86_64
    * Updated wheel openssl version. Updated build scripts
    * Updated ci cfgs
    * Updated dockerfiles
    * Updated setup.py

--- ci/build-manylinux.sh
+++ ci/build-manylinux.sh
@@ -16,23 +16,44 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 
-docker_tag="parallelssh/ssh-manylinux"
-docker_files=("ci/docker/manylinux/Dockerfile" "ci/docker/manylinux/Dockerfile.2014_x86_64")
+docker_repo="parallelssh/ssh-manylinux"
+docker_files=(
+              "ci/docker/manylinux/Dockerfile"
+              "ci/docker/manylinux/Dockerfile.2014_x86_64"
+              "ci/docker/manylinux/Dockerfile.manylinux_2_24_x86_64"
+              "ci/docker/manylinux/Dockerfile.manylinux_2_28_x86_64"
+              )
 
 rm -rf local build ssh/libssh.* ssh/*.so
 python ci/appveyor/fix_version.py .
 
 if [[ $(uname -m) == "aarch64" ]]; then
-    docker_tag=${docker_tag}:aarch64
-    docker_files=("ci/docker/manylinux/Dockerfile.aarch64")
+    docker_files=(
+                  "ci/docker/manylinux/Dockerfile.aarch64"
+                  "ci/docker/manylinux/Dockerfile.aarch64_2_24"
+                  "ci/docker/manylinux/Dockerfile.aarch64_2_28"
+                  )
 fi
 
 for docker_file in "${docker_files[@]}"; do
     if [[ ${docker_file} == "ci/docker/manylinux/Dockerfile_2014_x86_64" ]]; then
-        docker_tag="${docker_tag}:2014_x86_64"
+        docker_tag="${docker_repo}:2014_x86_64"
+    elif [[ ${docker_file} == "ci/docker/manylinux/Dockerfile" ]]; then
+        docker_tag="${docker_repo}:2010_x86_64"
+    elif [[ ${docker_file} == "ci/docker/manylinux/Dockerfile.manylinux_2_24_x86_64" ]]; then
+        docker_tag="${docker_repo}:2_24_x86_64"
+    elif [[ ${docker_file} == "ci/docker/manylinux/Dockerfile.manylinux_2_28_x86_64" ]]; then
+        docker_tag="${docker_repo}:2_28_x86_64"
+    elif [[ ${docker_file} == "ci/docker/manylinux/Dockerfile.aarch64" ]]; then
+        docker_tag="${docker_repo}:2014_aarch64"
+    elif [[ ${docker_file} == "ci/docker/manylinux/Dockerfile.aarch64_2_24" ]]; then
+        docker_tag="${docker_repo}:2_24_aarch64"
+    elif [[ ${docker_file} == "ci/docker/manylinux/Dockerfile.aarch64_2_28" ]]; then
+        docker_tag="${docker_repo}:2_28_aarch64"
     fi
+    echo "Docker tag is ${docker_tag}"
     docker pull $docker_tag || echo
-    docker build --pull --cache-from $docker_tag ci/docker/manylinux -t $docker_tag -f ${docker_file}
+    docker build --pull --cache-from $docker_tag ci/docker/manylinux -t $docker_tag -f "${docker_file}"
     if [[ -z "${CIRCLE_PULL_REQUEST}" ]]; then docker push $docker_tag; fi
     docker run --rm -v "$(pwd)":/io $docker_tag /io/ci/build-wheels.sh
     ls wheelhouse/
